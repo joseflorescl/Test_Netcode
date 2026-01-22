06cd14c78f7aa0302338dff7e886eae01bfb972a
Jose Leonardo Flores <joseflores.cl@gmail.com>
2026-01-22 20:11:45 -0300

    wip: TicTacToeMultiplayer: winner with line complete

--- Assets/TicTacToeMultiplayer/Scripts/GameManager.cs
+++ Assets/TicTacToeMultiplayer/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -11,13 +13,27 @@ public class GameManager : NetworkBehaviour
     public event EventHandler<OnClickedOnGridPositionEventArgs> OnClickedOnGridPosition;
     public event EventHandler OnGameStarted;
     public event EventHandler OnCurrentPlayablePlayerTypeChanged;
+    public event EventHandler<OnGameWinEventArgs> OnGameWin;
+
+    public class OnGameWinEventArgs : EventArgs
+    {
+        public Line line;
+    }
 
     public class OnClickedOnGridPositionEventArgs: EventArgs
     {
         public int x;
         public int y;
         public PlayerType playerType;
-    }    
+    }
+
+    public enum LineType { Horizontal, Vertical, Diagonal }
+    public struct Line
+    {
+        public LineType lineType;
+        public int lineIndex;
+        public Vector2Int centerGridPosition;
+    }
 
     PlayerType localPlayerType;
     NetworkVariable<PlayerType> currentPlayablePlayerType = new(value: PlayerType.None);
@@ -26,6 +42,7 @@ public class GameManager : NetworkBehaviour
     public PlayerType CurrentPlayablePlayerType => currentPlayablePlayerType.Value;
 
     PlayerType[,] playerTypeArray;
+    List<Line> lineList;
 
     private void Awake()
     {
@@ -35,6 +52,25 @@ public class GameManager : NetworkBehaviour
         }
         Instance = this;
         playerTypeArray = new PlayerType[3, 3];
+
+        InitLineList();
+    }
+
+    void InitLineList()
+    {
+        lineList = new List<Line>()
+        {
+            new Line() { lineType = LineType.Horizontal, lineIndex = 0, centerGridPosition = new Vector2Int(1,0) },
+            new Line() { lineType = LineType.Horizontal, lineIndex = 1, centerGridPosition = new Vector2Int(1,1) },
+            new Line() { lineType = LineType.Horizontal, lineIndex = 2, centerGridPosition = new Vector2Int(1,2) },
+            //
+            new Line() { lineType = LineType.Vertical, lineIndex = 0, centerGridPosition = new Vector2Int(0,1) },
+            new Line() { lineType = LineType.Vertical, lineIndex = 1, centerGridPosition = new Vector2Int(1,1) },
+            new Line() { lineType = LineType.Vertical, lineIndex = 2, centerGridPosition = new Vector2Int(2,1) },
+            //
+            new Line() { lineType = LineType.Diagonal, lineIndex = 0, centerGridPosition = new Vector2Int(1,1) },
+            new Line() { lineType = LineType.Diagonal, lineIndex = 2, centerGridPosition = new Vector2Int(1,1) },
+        };
     }
 
     public override void OnNetworkSpawn()
@@ -117,24 +153,72 @@ public class GameManager : NetworkBehaviour
 
     void TestWinner()
     {
-        if (TestWinnerLine(0))// bottom row
+        foreach (var line in lineList)
         {
-            print("Winner");
+            if (TestWinnerLine(line.lineIndex, line.lineType))
+            {
+                print("Winner");
+                currentPlayablePlayerType.Value = PlayerType.None;
+                OnGameWin?.Invoke(this, new OnGameWinEventArgs
+                {
+                    line = line
+                });
+                break;
+            }
         }
-    }
 
-    bool TestWinnerLine(int lineIdx)
+        
+    }    
+
+    bool TestWinnerLine(int lineIdx, LineType lineType)
     {
 
-        var firstValue = playerTypeArray[0, lineIdx];
+        PlayerType firstValue;
+        switch (lineType)
+        {
+            default:
+            case LineType.Horizontal:
+            case LineType.Diagonal:
+                firstValue = playerTypeArray[0, lineIdx];
+                break;
+            case LineType.Vertical:
+                firstValue = playerTypeArray[lineIdx, 0];
+                break;
+        }
+        
         if (firstValue == PlayerType.None)
             return false;
 
-        for (int i = 1; i < playerTypeArray.GetLength(1); i++)
+        if (lineType == LineType.Horizontal || lineType == LineType.Vertical)
         {
-            var item = playerTypeArray[i, lineIdx];
-            if (item != firstValue)
-                return false;
+            for (int i = 1; i < playerTypeArray.GetLength(1); i++) // Como la grilla es de 3x3 basta con un solo for.
+            {
+                PlayerType item;
+                switch (lineType)
+                {
+                    default:
+                    case LineType.Horizontal:
+                        item = playerTypeArray[i, lineIdx];
+                        break;
+                    case LineType.Vertical:
+                        item = playerTypeArray[lineIdx, i];
+                        break;
+                }
+                if (item != firstValue)
+                    return false;
+            }
+        }
+        else // Diagonal
+        {
+            int currentY = lineIdx;
+            int deltaY = lineIdx == 0 ? +1 : -1;
+            for (int i = 1; i < playerTypeArray.GetLength(1); i++)
+            {
+                currentY = currentY + deltaY;
+                var item = playerTypeArray[i, currentY];
+                if (item != firstValue)
+                    return false;
+            }
         }
 
         return true;
--- Assets/TicTacToeMultiplayer/Scripts/GameVisualManager.cs
+++ Assets/TicTacToeMultiplayer/Scripts/GameVisualManager.cs
@@ -6,23 +6,58 @@ public class GameVisualManager : NetworkBehaviour
     private const float GRID_SIZE = 4f;
 
     [SerializeField] Transform crossPrefab;
-    [SerializeField] Transform circlePrefab;   
-    
+    [SerializeField] Transform circlePrefab;
+    [SerializeField] Transform lineCompletePrefab;
+
 
     private void Start()
     {
         GameManager.Instance.OnClickedOnGridPosition += OnClickedOnGridPosition;
+        GameManager.Instance.OnGameWin += OnGameWin;
     }
 
     private void OnDisable()
     {
         GameManager.Instance.OnClickedOnGridPosition -= OnClickedOnGridPosition;
+        GameManager.Instance.OnGameWin -= OnGameWin;
     }
 
     private void OnClickedOnGridPosition(object sender, GameManager.OnClickedOnGridPositionEventArgs e)
     {
         SpawnObjectRpc(e.x, e.y, e.playerType);
     }
+
+    private void OnGameWin(object sender, GameManager.OnGameWinEventArgs e)
+    {
+        float eulerZ = 0;
+        switch (e.line.lineType)
+        {
+            case GameManager.LineType.Horizontal:
+                eulerZ = 0;
+                break;
+            case GameManager.LineType.Vertical:
+                eulerZ = 90;
+                break;
+            case GameManager.LineType.Diagonal:
+                if (e.line.lineIndex == 0)
+                {
+                    eulerZ = 45;
+                }
+                else
+                {
+                    eulerZ = -45;
+                }
+                    break;
+            default:
+                break;
+        }
+
+        Vector2 pos2D = GetGridWorldPosition(e.line.centerGridPosition.x, e.line.centerGridPosition.y);
+        Vector3 pos3D = new Vector3(pos2D.x, pos2D.y, lineCompletePrefab.position.z);
+        Transform line = Instantiate(lineCompletePrefab, pos3D, Quaternion.Euler(0, 0, eulerZ));
+        line.GetComponent<NetworkObject>().Spawn(true);
+    }
+
 
     [Rpc(SendTo.Server)]
     void SpawnObjectRpc(int x,  int y, PlayerType playerType)
